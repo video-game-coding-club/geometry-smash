07f18d6f5c654499a7af213083abeb089634375e
Nicolas Bock <nicolasbock@gmail.com>
2018-05-12 08:34:28 -0400

    Simple animation

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-/* Get cavas context. */
-let contextGame = document.getElementById("game-layer").getContext("2d");
+var canvas = document.getElementById("game-layer");
+var ctx = canvas.getContext("2d");
+
+var x = 0;
+
+var background = function(color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);  
+};
 
 var draw = function() {
-  //window.requestAnimationFrame(draw);
+  window.requestAnimationFrame(draw);
+  background("cornsilk");
+  
+  ctx.fillStyle = "black";
+  ctx.fillRect(x, 30, 10, 10);
+  
+  x += 1;
+  
+  if(x > canvas.width - 10) {
+    x = -10;
+  }
 };
 
 draw();
